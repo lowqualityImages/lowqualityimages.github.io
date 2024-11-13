7c8b7b1189d2ea8f17965fa684c0cb7559845cc6
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-11-13 18:18:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const input = document.getElementById('input');
 const object = document.getElementById('object');
 const startVid = document.getElementById('startVid');
 
+const audios = [{path: "audio/audio1.mp3", drop: 4500}, {path: "audio/audio2.mp3", drop: 4100}]
+
 const reader = new FileReader();
 
 input.addEventListener("change", () => {
@@ -13,14 +15,15 @@ input.addEventListener("change", () => {
 })
 
 startVid.addEventListener('click', () => {
+  const chosenAudio = audios[Math.floor(Math.random() * audios.length)]
   startVid.style.display = "none";
   object.style.display = "none";
-  const audio = new Audio(`audio/audio1.mp3`)
+  const audio = new Audio(chosenAudio.path)
   audio.play()
   audio.onplay = () => {
     setTimeout(() => {
       object.style.display = "initial";
-    }, 4500)
+    }, chosenAudio.drop)
   }
   audio.onended = () => {
     startVid.style.display = "initial";
